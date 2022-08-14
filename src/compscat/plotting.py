d1ba33e4db2e504bd920572d5178968bf0ec893c
amanmdesai <amanmukeshdesai@gmail.com>
2022-08-14 18:47:36 +0530

    add note book

--- src/compscat/plotting.py
+++ src/compscat/plotting.py
@@ -4,22 +4,21 @@ import uproot
 
 class PlotData:
 	
-    def __init__(self):
-    	self.filename=filename    
+
     def plot(data, key):
-        plt.hist(data, bins=100, color=None)
+        plt.hist(data, bins=40, color=None)
         label = key.replace("_", " ")
         plt.xlabel(label + " [keV]")
         plt.ylabel("Counts")
         plt.show()
         plt.savefig(key + ".png")
 
-    def file(self.filename='MC_compton.root'):
+    def file(filename='MC_compton.root'):
         if 'root' not in filename:
-            raise Exception("Input must be a ROOT file")
-        file = uproot.open(self.filename)
+            raise TypeError("Input must be a ROOT file")
+        file = uproot.open(filename)
         tree = file["events"]
         branches = tree.arrays()
         for key in tree.keys():
-            PlotData().plot(branches[key], key)
-        return 0
+            PlotData.plot(branches[key], key)
+
