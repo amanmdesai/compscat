c7b7f76395c929b7c8981e7367576a06d1e8bb3a
amanmdesai <amanmukeshdesai@gmail.com>
2023-01-10 15:58:07 +0530

    update test

--- tests/test_xsec.py
+++ tests/test_xsec.py
@@ -1,21 +1,20 @@
-from __future__ import annotations
+import compscat
 
-from compscat import *
+def test_first():
+    E = 0.1
+    N = 100000
+    w_sum, w_square, w_max = compscat.CrossSection(E / compscat.constants.m).integrate_xsec(
+        N
+    )
 
-E = 0.1
-N = 100000
-w_sum, w_square, w_max = compscat.CrossSection(E / compscat.constants.m).integrate_xsec(
-    N
-)
+    # print(constants.convert)
 
-# print(constants.convert)
+    sigma_x = w_sum * compscat.constants.convert / (N * 1e12)
 
-sigma_x = w_sum * compscat.constants.convert / (N * 1e12)
+    print(sigma_x)
 
-print(sigma_x)
-
-assert sigma_x < 0.0095
-assert sigma_x > 0.0076
+    assert sigma_x < 0.0095
+    assert sigma_x > 0.0076
 #    print("test_passed")
 # else:
 #    raise(Error)
